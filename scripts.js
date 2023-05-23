41e75926b74cbb9352887d8eff0c270a4b858df6
fjarm <129107554+fjarm@users.noreply.github.com>
2023-05-23 12:50:13 -0400

    Create operation function and setup variables

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let currentOperator = undefined;
+
 function add(x, y)
 {
     return x + y;
@@ -16,4 +20,25 @@ function divide(x, y)
 function multiply(x, y)
 {
     return x * y
+}
+
+function operate(x, y, operator)
+{
+    switch(operator)
+    {
+        case "add":
+            add(x, y);
+            break;
+        case "subtract":
+            subtract(x, y);
+            break;
+        case "divide":
+            divide(x, y);
+            break;
+        case "multiply":
+            multiply(x, y);
+            break;
+        default:
+            return;
+    }
 }
