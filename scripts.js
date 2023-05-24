77082131b8dbf37bad1f8c33124e81b8c8624e46
fjarm <129107554+fjarm@users.noreply.github.com>
2023-05-24 12:11:33 -0400

    Add variable initializers

--- scripts.js
+++ scripts.js
@@ -2,6 +2,27 @@ let firstNumber = 0;
 let secondNumber = 0;
 let currentOperator = undefined;
 
+const Zero  = document.querySelector("#0")
+const One   = document.querySelector("#1");
+const Tw0   = document.querySelector("#2");
+const Three = document.querySelector("#3");
+const Four  = document.querySelector("#4");
+const Five  = document.querySelector("#5");
+const Six   = document.querySelector("#6");
+const Seven = document.querySelector("#7");
+const Eight = document.querySelector("#8");
+const Nine  = document.querySelector("#9");
+
+const clear = document.querySelector("clear");
+const toggleNegative = document.querySelector("#plusminus");
+const percent = document.querySelector("percent");
+
+const plusButton = document.querySelector("plus");
+const subButton = document.querySelector("minus");
+const multiplyButton = document.querySelector("multiply");
+const divideButton = document.querySelector("divide");
+const equalsButton = document.querySelector("equals");
+
 function add(x, y)
 {
     return x + y;
